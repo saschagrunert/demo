6489725e1b49ff73af377eb97e377b5f2a1ee60a
Sascha Grunert <mail@saschagrunert.de>
2019-12-23 11:19:43 +0100

    Enhance demo unit tests
    Signed-off-by: Sascha Grunert <mail@saschagrunert.de>

--- demo_test.go
+++ demo_test.go
@@ -9,12 +9,51 @@ import (
 )
 
 var _ = t.Describe("Demo", func() {
-	It("should succeed to run", func() {
-		// Given
-		sut := demo.New()
+	BeforeEach(func() {
 		os.Args = []string{
 			"demo", "--auto", "--immediate",
 		}
+	})
+
+	It("should succeed to run", func() {
+		// Given
+		sut := demo.New()
+
+		// When
+		sut.Run()
+	})
+
+	It("should succeed to run with example", func() {
+		// Given
+		example := func() *demo.Run {
+			r := demo.NewRun(
+				"Title",
+				"Some additional",
+				"multiline description",
+			)
+
+			r.Step(demo.S(
+				"This is a possible",
+				"description of the command",
+				"to be executed",
+			), demo.S(
+				"echo hello world",
+			))
+
+			// Commands to not need to have a description
+			r.Step(nil, demo.S(
+				"echo without description",
+			))
+
+			// It is also not needed to provide a command
+			r.Step(demo.S(
+				"Just a description without a command",
+			), nil)
+
+			return r
+		}
+		sut := demo.New()
+		sut.Add(example(), "title", "description")
 
 		// When
 		sut.Run()
